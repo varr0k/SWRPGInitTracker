e6c1f83cb7a531e093119235ee31c3611ec3e950
varrok <varrok@protonmail.com>
2022-05-20 20:15:07 -0400

    Add JsonConverter and very basic display of Abilities, Talents, Gear, Skills

---
+++ SWRPGInitTracker/Helpers/DiceSymbolHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace SWRPGInitTracker.Helpers
+{
+    public static class DiceSymbolHelper
+    {
+        private static readonly string _htmlBoost = "<span class=\"swicon boost\"></span>";
+
+        public static string Format(string input)
+        {
+            return input.Replace(":boost:", _htmlBoost);
+        }
+    }
+}
